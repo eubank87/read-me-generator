20c08c8fa87ae3a59f80e73d0f1cec0b55cd6ebf
eubank87 <eubank.ashley@gmail.com>
2021-01-07 15:26:36 -0800

    made sure git ignore was correct

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,10 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
+// Create an array of questions for user input
 const questions = [
     {
         type: 'input',
