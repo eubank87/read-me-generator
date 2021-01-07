8ecc065424bd0cb5062ffdd373410e26619b4f12
eubank87 <eubank.ashley@gmail.com>
2021-01-06 17:24:56 -0800

    began initial stages of readme development: installed packages, wrote out input/list questions

---
+++ Develop/index.js
@@ -0,0 +1,66 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project Title?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Enter a brief description of your project.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What is the usage information?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What are the contribution guidelines?',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions?',
+        name: 'test',
+    },
+    {
+        type: 'list',
+        message: 'Which License did you use?',
+        name: 'license',
+        choices: ['MIT', 'Apche 2.0', 'GPL 3.0', 'BSD3'],
+    },
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    inquirer.prompt(questions)
+}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
